3647df924956ea869526c78881c341c69d01c499
bongjunk <mydlwm_s@naver.com>
2023-11-07 11:29:28 +0900

    firebase set

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app'
-import { getAnalytics } from 'firebase/analytics'
+import { getApps, initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore/lite';
+import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
@@ -10,8 +11,14 @@ const firebaseConfig = {
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENTID,
-}
+};
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig)
-const analytics = getAnalytics(app)
+// !getApps().length: nextjs app을 리로드 할 때 SDK 다시 초기화 방지
+export const firebase_app = !getApps().length
+  ? initializeApp(firebaseConfig)
+  : getApps()[0];
+export const firebase_db = getFirestore(firebase_app);
+const analytics = getAnalytics(app);
+
+// export { firebase_app };
